c800bdce84ca19aa344512f4366ddcb93737c07b
Reuben Wisdom Ikedichi <100050689+Rublyn11@users.noreply.github.com>
2023-11-13 22:51:13 +0100

    Add files via upload
    Changes of new sections added to the website

--- scripts/app.js
+++ scripts/app.js
@@ -30,3 +30,53 @@ datas.forEach((data)=>{
     `
 
 })
+
+
+let involved_grid = document.querySelector('.involved_grid');
+
+info.forEach((data)=>{
+    involved_grid.innerHTML += `
+
+    <div class="involved_main">
+        <h1 class="titled">${data.titled}</h1>
+        <p class="texted">${data.texted}</p>
+    </div>
+    
+    `
+})
+
+
+let stake_grid = document.querySelector('.stake_grid');
+
+team.forEach((data)=>{
+    stake_grid.innerHTML += `
+
+    <div class="stake_card_main">
+        <div class="stake_card_img">
+            <img src="${data.picture}">
+                </div>
+                    
+                <div class="stake_card_text">
+                    <h3>${data.name}</h3>
+                    <small>${data.role}</small>
+                </div>
+        </div>
+    </div>
+    
+    `
+})
+
+let global_grid = document.querySelector('.global_grid');
+
+farmer.forEach((data)=> {
+    global_grid.innerHTML += `
+
+    <div class="global_main">
+        <div class="global_image">
+            <img src="${data.image}" alt="">
+        </div>
+            <small>${data.small}</small>
+    </div>
+
+    `
+})
--- scripts/data.js
+++ scripts/data.js
@@ -1,22 +1,87 @@
 const datas = [
     {
         img: "./images/Water downfall.jpeg",
-        title: 'Endotermic Surounding',
-        para: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid architecto at deleniti aspernatur. Molestiae aperiam iure porro, nihil non sunt?"
+        title: 'Environmental Stewardship:',
+        para: "We believe in humanity's capacity to act as responsible stewards of the Earth, working together to protect and preserve the diverse ecosystems that make our planet unique."
     },
     {
         img: "./images/butterfly.jpeg",
-        title: 'Endotermic Surounding',
-        para: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid architecto at deleniti aspernatur. Molestiae aperiam iure porro, nihil non sunt?"
+        title: 'Sustainability:',
+        para: " Our vision encompasses a global commitment to sustainable practices that ensure the planet's resources are used wisely, benefiting both current and future generations."
     },
     {
         img: "./images/Kinds in garden.jpg",
-        title: 'Endotermic Surounding',
-        para: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid architecto at deleniti aspernatur. Molestiae aperiam iure porro, nihil non sunt?"
+        title: 'Education and Awareness:',
+        para: "Our vision includes a society well-informed about the impacts of their choices on the environment, enabling everyone to make more sustainable decisions."
     },
     {
         img: "./images/Growing flower.webp",
-        title: 'Endotermic Surounding',
-        para: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquid architecto at deleniti aspernatur. Molestiae aperiam iure porro, nihil non sunt?"
+        title: 'Innovation:',
+        para: " We see a future where innovative technologies and solutions play a pivotal role in addressing environmental issues, making it possible to reduce our ecological footprint."
+    }
+]
+
+const info = [
+    {
+        titled: "Plant More Trees",
+        texted: "Join our tree-planting initiative to foster a greener planet."
+    },
+    {
+        titled: "Clean Energy Sources",
+        texted: "Switch to renewable energy solutions to reduce carbon emissions."
+    },
+    {
+        titled: "Sustainable Transport",
+        texted: "Promote green transportation methods for a cleaner Earth."
+    },
+    {
+        titled: "Recycling Campaigns",
+        texted: "Join our community-driven recycling programs to reduce waste."
+    },
+    {
+        titled: "Resource Conservation",
+        texted: "Adopt an eco-conscious lifestyle for a better tomorrow."
+    },
+    {
+        titled: "Biodegradable Packaging",
+        texted: "Our movement promotes sustainable packaging options."
+    }
+]
+
+const team = [
+    {
+        picture: "./images/team_two.png",
+        name: "Brenda Adams",
+        role: "Founder"
+    },
+    {
+        picture: "./images/team_three.png",
+        name: "Adekunle Moses",
+        role: "Animal Activist"
+    },
+    {
+        picture: "./images/team_four.png",
+        name: "Lois Coker",
+        role: "Botanist"
+    },
+    {
+        picture: "./images/team_one.png",
+        name: "Davie Richie",
+        role: "Environmental Strategist"
+    }
+]
+
+const farmer = [
+    {
+        image: "./images/farmers.png",
+        small: "Planting new trees-Finland"
+    },
+    {
+        image: "./images/vetenary.png",
+        small: "Animals Matter Campaign-South Africa"
+    },
+    {
+        image: "./images/beach.png",
+        small: "No_more_sea_Garbage- Nigeria"
     }
 ]
